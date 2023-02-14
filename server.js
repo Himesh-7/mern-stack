39581a3e554ae4a0f41f48f205401d1f73950376
Himesh-7 <himeshthungaturthi2@gmail.com>
2023-02-14 13:08:41 +0530

    original

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
 
-app.get("/index", function(req, res){
+app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html");
 });
 
