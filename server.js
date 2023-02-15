43571c7b20681a41a6c948ec1ed9aae93614449a
Himesh-7 <himeshthungaturthi2@gmail.com>
2023-02-15 22:19:12 +0530

    done changes

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
+const mongoose=require("mongoose");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
-
+mongoose.set('strictQuery', true);
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/Resume.html");
 });
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+	if(err)
+	{
+		console.error(err);
+	}
+	else
+	{
+		console.log("DB Connected");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+	}
 });
+
+
