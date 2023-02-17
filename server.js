57e6c07544b78e0cfaee24ebf44689aa9f1fc4fa
Himesh-7 <himeshthungaturthi2@gmail.com>
2023-02-17 14:53:09 +0530

    added few

--- server.js
+++ server.js
@@ -1,60 +1,86 @@
-require("dotenv").config();
-const mongoose=require("mongoose");
+require('dotenv').config();
+const userLib = require("./backend/lib/userLib");
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 5030;
 const options = {
     extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
     index: ['index.html'],  
 }
-app.use(express.static("public", options));
-const userLib= require("./backend/libs/userlib");
+app.use(express.static("public", options));
 
+// app.use(express.static(__dirname + '/public'));
 
+app.get('/card', function(req, res) {
+    res.sendFile(__dirname + '/card.html');
+});
+app.get('/index', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/Resume', function(req, res) {
+    res.sendFile(__dirname + '/Resume.html');
+});
 mongoose.set('strictQuery', true);
-
-app.get("/card", function(req, res){
-	res.sendFile(__dirname+"/card .html");
-});
-app.get("/", function(req, res){
-	res.sendFile(__dirname+"/index.html");
-});
-app.get("/Resume", function(req, res){
-	res.sendFile(__dirname+"/Resume.html");
-});
-mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
-	if(err)
-	{
-		console.error(err);
-	}
-	else
-	{
-		console.log("DB Connected");
-		
-		//userLib.createFirstUser(function(err,res){
-		//	if(err)
-		//	{
-		//		//console.error(err);
-		//	}
-		//	else
-		//	{
-		//		console.log(res);
-		//	}
-		//});
-		userLib.updateUser(function(err,result){
-			if(err)
-			{
-				console.log(err);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Connected to database');
+        // TODO : donot create a user if atleast 1 user exist in the table
+        //    userLib.createFirstUser(function(err,result){
+		//  	if(err){
+		//  		// console.error(err);
+		//  	}
+		//  	else{
+		//  		console.log(result);
+		//  	}
+		//  });
+		// userLib.createUser({userName: "beingzero", yearOfGraduation: 2025},function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.updateUser(function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		/*userLib.deleteUser("Saivenkat",function(err,result){
+			if(err){
+				console.error(err);
 			}
-			else
-			{
+			else{
 				console.log(result);
 			}
-		});
+		});*/
+		//   userLib.getUserByFilter({userName: "saivenkat"}, function(err,result){
+		// 	if(err){
+		//  		console.error(err);
+		// 	}
+		//  	else{
+		// 		console.log(result);
+		// 	}
+		//  });
+		 userLib.getAllUsers(function(err,result){
+		 	if(err){
+		 		console.error(err);
+		 	}
+		 	else{
+		 		console.log(result);
+		 	}
+		 });
+
 		app.listen(port, function(){
-			console.log('Server started on port '+port);
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
 		});
+		
 	}
 });
-
-
