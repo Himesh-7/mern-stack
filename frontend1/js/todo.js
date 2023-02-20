643b3cc491510264843cb215b9203a2f2c2cd212
Himesh-7 <himeshthungaturthi2@gmail.com>
2023-02-20 19:52:55 +0530

    commit

--- frontend1/js/todo.js
+++ frontend1/js/todo.js
@@ -1,24 +1,26 @@
 console.log("hello from js");
-
+document.getElementById("loader").style.display="block";
 fetch("/api/todos")
     .then(function(res) {
         return res.json();
     })
     .then(function(data) {
         console.log(data);
+        document.getElementById("loader").style.display="none";
+        document.getElementById('todoCount').innerHTML=data.length;
     });
 
 // change an attribute of html tag 
 var light = true;
 
-// change innerhtml of themebutton to Sun icon
+// change innerhtml of Theme Button to Sun icon
 function setTheme() {
     if (light) {
         document.documentElement.setAttribute("data-bs-theme", "dark");
-        document.getElementById("themeButton").innerHTML = '<i class="fas fa-sun fa-lg fa-fw"></i>';
+        
     } else {
         document.documentElement.setAttribute("data-bs-theme", "light");
-        document.getElementById("themeButton").innerHTML = '<i class="fas fa-moon fa-lg fa-fw"></i>';
+      
     }
     light = !light;
 }
